16f3717b6db8e28dc0aad31ebaadec5e38f8ae0a
Simon Gooder <simgooder@gmail.com>
2016-03-19 19:59:07 -0400

    first commit. site in ready state

---
+++ site/js/scripts.js
@@ -0,0 +1,51 @@
+
+
+$(document).ready(function(){
+
+	$('.js-toggle-modal').click(function(){
+		$('.modal--overlay').toggleClass('--is--open');
+	});
+
+	$('.icon--camera__left').click(function(){
+		//$().addClass('TEST-CLASS');
+		var next = $(this).next()[0];
+		$(next).toggleClass('-hide');
+	});
+
+
+});
+
+// Twitter Widget
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
+
+// Facebook SDK
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '474954152698654',
+      xfbml      : true,
+      version    : 'v2.5'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
+
+// Google Analytics
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-73741436-2', 'auto');
+  ga('send', 'pageview');
+
